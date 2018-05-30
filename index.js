1f26a5d57abfbceee27240a75b99976aadea14b3
ygroxie <38132498+ygroxie@users.noreply.github.com>
2018-05-30 15:38:48 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var bot = new Discord.Client();
 var prefix = ("?")
 
 bot.on("ready", function() {
-    bot.user.setGame("YgroxBot, ?help");
-    console.log("Le bot a bien, était connecté !");
+    bot.user.setGame("E-NETWORK");
+    console.log("Le bot a bien était connecté !");
 });
 
 bot.on('message', function (message) {
@@ -16,24 +16,24 @@ bot.on('message', function (message) {
 });
 
 bot.on('guildMemberAdd', member => {
-    var role = member.guild.roles.find('name', 'Joueur');
+    var role = member.guild.roles.find('name', 'Constructeur');
     member.addRole(role)
 })
 
 bot.on('guildMemberAdd', member => {
-    member.guild.channels.find("name", "bienvenue-aurevoir").send(`Bienvenue à toi ${member} sur le serveur de la GFX Communauté !`)
-})
-
-bot.on('guildMemberRemove', member => {
-
-    member.guild.channels.find("name", "bienvenue-aurevoir").send(`Oh non ! ${member.user} vient de quitter le serveur !`)
-
+    var bvn_embed = new Discord.RichEmbed()
+    .setColor('#E81414')
+    .setImage(member.user.displayAvatarURL)
+    .addField("Bienvenue", `Bienvenue ${member.user.username} sur E-NETWORK nous somme actuellement ${member.guild.memberCount} membres`)
+    .setFooter(`${member.user.username}`)
+    .setTimestamp()
+    member.guild.channels.find("name", "✔-discussion").send(bvn_embed)
 })
 
 bot.on('message',(message)=>{
     if(message.content == "?help") {
     var embed = new Discord.RichEmbed()
-    .addField("?help","affiche les commandes du bot. ")
+    .addField("?help","affiche les commandes du bot.")
     .setColor("D7F705")
     .setTitle("Guide Commandes !")
     .setFooter ("(Utile si tu as courtes mémoire ! x) ")
@@ -48,8 +48,10 @@ bot.on('message', message => {
 
     if (command === "kick") {
         let modRole = message.guild.roles.find("name", "Modo");
-        if(!message.member.roles.has(modRole.id)) {
+        if(message.member.hasPermission("KICK_MEMBER")) {
             return message.reply("Tu n'as pas la permission de faire cette commande. Désolé !").catch(console.error);
+            
+                }
         }
         if(message.mentions.users.size === 0) {
             return message.reply("Merci de mentionner l'utilisateur à expulser.").catch(console.error);
@@ -62,22 +64,24 @@ bot.on('message', message => {
             return message.reply("Je n'ai pas la permission KICK_MEMBERS pour faire ceci.").catch(console.error);
         }
         kickMember.kick().then(member => {
-            message.reply(`${member.user.username} a été expulsé avec succès !`).catch(console.error);
-            message.guild.channels.find("name", "bienvenue-aurevoir").send(`**${member.user.username} a été expulsé du discord par **${message.author.username}**`);
+            message.reply(`${member.user.username} a été expulsé avec succès ${message;author.username} !`).catch(console.error);
+            message.guild.channels.find("name", "réponse-bot").send(`**${member.user.username} a été expulsé du discord par **${message.author.username}**`);
         }).catch(console.error)
         
 }
 
 if (command === "ban") {
     let modRole = message.guild.roles.find("name", "Modo");
-    if(!message.member.roles.has(modRole.id)) {
+    if(message.member.hasPermission("BAN_MEMBER")) {
         return message.reply("Tu n'as pas la permission de faire cette commande. Désolé !").catch(console.error);
+        
+            }
     }
     const member = message.mentions.members.first();
     if (!member) return message.reply("Merci de mentionner l'utilisateur à bannir.");
     member.ban().then(member => {
-        message.reply(`${member.user.username} a été bannu avec succès !`).catch(console.error);
-        message.guild.channels.find("name", "bienvenue-aurevoir").send(`**${member.user.username}** a été banni du discord par **${message.author.username}**`);
+        message.reply(`${member.user.username} a été banni avec succès ${message.author.username} !`).catch(console.error);
+        message.guild.channels.find("name", "réponse-bot").send(`**${member.user.username}** a été banni du discord par **${message.author.username}**`);
     }).catch(console.error)
 }})
 
